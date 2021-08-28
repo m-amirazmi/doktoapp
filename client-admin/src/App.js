6fb88d2b3a966b486181069f5349ae1944145414
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-08-28 17:44:35 +0800

    ADMIN => Initial setup with dependancies installation

---
+++ client-admin/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      This is a doktoapp
+      <button className="p-3 bg-blue-500">This is button</button>
+    </div>
+  )
+}
---
+++ client-admin/src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './assets/styles/index.css'
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
